63898ea909448fbc2ce14bfb4c99208945cffbab
Becky Zahid <buzahid@microsoft.com>
2024-02-15 04:53:43 -0800

    Guided project solution and starter code

---
+++ M04-Guided-Project/Starter/WPF/JSON/GameJSONSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json;
+using LangtonsAnt;
+
+namespace Json
+{
+    public static class GameJSONSerializer
+    {
+        public static string? ToJson(IGame game)
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters = {
+                    new MultiDimensionalArrayConverter(),
+                    //new InterfaceConverterFactory(typeof(GeneralizedAnt), typeof(IAnt)),
+                }
+            };
+            
+            return null;
+        }
+
+        public static IGame? FromJson(string jsonString)
+        {
+            IGame? game = null;
+            var serializeOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters = {
+                    new MultiDimensionalArrayConverter(),
+                    //new InterfaceConverterFactory(typeof(GeneralizedAnt), typeof(IAnt)),
+                }
+            };
+
+            return game;
+        }
+    }
+}
